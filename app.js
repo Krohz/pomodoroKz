a28dcfa7321b3bf498153996f0683b95ce5b23cd
Kros <cristian@alvias.com>
2022-08-12 12:35:56 -0500

    terminado

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//variables
+//variables - AC
 const tasks = [];
 let time = 0;
 let timer = null;
@@ -8,6 +8,10 @@ let current = null;
 const bAdd = document.querySelector("#bAdd");
 const itTask = document.querySelector("#itTask");
 const form = document.querySelector("#form");
+const taskName = document.querySelector("#time #taskName");
+
+renderTime();
+renderTarea();
 
 //Eventos
 Eventlistener();
@@ -26,12 +30,13 @@ function validarForm(e){
     }
 }
 
-function crearTarea(valor){
+function crearTarea(value){
     const newTask = {
-        id: Date.now(),
-        title : valor,
-        completed : false,
-    }
+        id: (Math.random() * 100).toString(36).slice(2),
+        title: value,
+        completed: false,
+    };
+    
     tasks.unshift(newTask);
 }
 
@@ -47,4 +52,77 @@ function renderTarea(){
 
     const taskContainer = document.querySelector("#tasks");
     taskContainer.innerHTML = html.join("");
+
+    const startButtons = document.querySelectorAll(".task .start-button");
+
+    startButtons.forEach(button =>{
+        button.addEventListener("click", e=>{
+            if (!timer) {
+                const id = button.getAttribute("data-id");
+                startButtonHandler(id);
+                button.textContent = "En progreso...";
+            }
+        });
+    });
+}
+
+function startButtonHandler(id){
+    time = 25*60;
+    current = id;
+    const taskId = tasks.findIndex((task) => task.id === id);
+    document.querySelector("#time #taskName").textContent = tasks[taskId].title;
+    renderTime();
+
+    timer = setInterval(()=>{
+        timeHandler(id);
+    },1000);
+}
+
+function timeHandler(id){
+    time--;
+    renderTime();
+
+    if (time === 0) {
+        clearInterval(timer);
+        markCompleted(id);
+        timer = null;
+        renderTarea();
+        startBreak();
+    }
+}
+
+function renderTime(){
+    const timeDiv = document.querySelector("#time #value");
+    const minutes = parseInt(time / 60);
+    const seconds = parseInt(time % 60);
+
+    timeDiv.textContent = `${minutes < 10 ? "0": "" }${minutes}:${seconds < 10 ? "0": "" }${seconds}`;
+}
+
+function markCompleted(id){
+    const taskIndex = tasks.findIndex((task) => task.id === id);
+    tasks[taskIndex].completed = true;
+}
+
+function startBreak(){
+    time = 5*60;
+    taskName.textContent = "Break";
+    renderTime();
+
+    timerBreak = setInterval(()=>{
+        timerBreakHandler();
+    },1000)
+}
+
+function timerBreakHandler(){
+    time--;
+    renderTime();
+
+    if (time === 0) {
+        clearInterval(timerBreak);
+        current = null;
+        timerBreak = null;
+        taskName.textContent = "";
+        renderTarea();
+    }
 }
